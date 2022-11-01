b1efa89eb4aad3241da561e75684bcff8ede8eb4
nope <108734462+nopetoyou@users.noreply.github.com>
2022-11-01 23:34:26 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,91 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+
+document.getElementById("btn").onclick = function(){
+    var audio = new Audio("play.mp3");
+    audio.play();
+}
+// ====================================================================
+
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+
+/**
+ * @param {Date} date
+ */
+function formatTime(date) {
+  const hours12 = date.getHours() % 12  || 12;
+  const minutes = date.getMinutes();
+  const isAm = date.getHours() < 12;
+
+  return `${hours12.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+}
+
+/**
+ * @param {Date} date
+ */
+function formatDate(date) {
+  const DAYS = [
+    "Sunday | ",
+    "Monday | ",
+    "Tuesday | ",
+    "Wednesday | ",
+    "Thursday | ",
+    "Friday | ",
+    "Saturday | "
+  ];
+  const MONTHS = [
+    "January | ",
+    "February | ",
+    "March | ",
+    "April | ",
+    "May | ",
+    "June | ",
+    "July | ",
+    "August | ",
+    "September | ",
+    "October | ",
+    "November | ",
+    "December"
+  ];
+
+  return `${DAYS[date.getDay()]}, ${
+    MONTHS[date.getMonth()]
+  } ${date.getDate()} ${date.getFullYear()}`;
+}
+
+setInterval(() => {
+  const now = new Date();
+
+//   timeElement.textContent = formatTime(now);
+  dateElement.textContent = formatDate(now);
+}, 200);
